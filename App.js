02137719b05c7eb362744402543646c6c5243af7
LGE\gyubin1.mun <gyubin1.mun@lgepartner.com>
2023-03-23 14:27:19 +0900

    basic setting

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from "@react-navigation/native";
+import {View} from "react-native";
+import Root from "./navigation/Root";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const RootNavigation = () => {
+    return (
+        <View style={{ flex: 1 }}>
+            <Root/>
+        </View>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+  function App () {
+    return (
+        <NavigationContainer independent={true}>
+          <RootNavigation />
+        </NavigationContainer>
+    );
+  }
+
+  export default App;
